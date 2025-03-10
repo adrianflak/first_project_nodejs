aa8a8e58491ed7b3b64e70673839007cf967c866
Adrian <kontakt@adrianflak.pl>
2025-03-10 12:00:24 +0100

    test2

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ const app = require("./app")
 const port = process.env.PORT || 3000
 
 app.listen(port, () =>
-	console.log("Aplikacja działa na porcie 3000")
+	console.log("Aplikacja działa na porcie 3000 Adrian")
 
 )
